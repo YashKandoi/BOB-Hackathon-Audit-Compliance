665fafe76cb7b7cdd360d7a0b24912727a77896f
Suvid Singhal <anooptest100@gmail.com>
2024-07-27 00:50:26 +0530

    Fixed path for regulations

--- complicanceAI/complicanceAI/views.py
+++ complicanceAI/complicanceAI/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-base_path = './regulations/'
+base_path = './regulations_files/'
 
 @api_view(['GET', 'POST'])
 def aml_guidelines(request):
