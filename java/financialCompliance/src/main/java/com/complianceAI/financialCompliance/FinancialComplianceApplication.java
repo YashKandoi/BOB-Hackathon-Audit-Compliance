80087de000661ed9c6fcf1c2e48675c2b12cde5b
Aditya Deshpande <f20212681@pilani.bits-pilani.ac.in>
2024-10-12 16:53:05 +0530

    initialised spring project

---
+++ java/financialCompliance/src/main/java/com/complianceAI/financialCompliance/FinancialComplianceApplication.java
@@ -0,0 +1,13 @@
+package com.complianceAI.financialCompliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialComplianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialComplianceApplication.class, args);
+	}
+
+}
---
+++ java/financialCompliance/src/test/java/com/complianceAI/financialCompliance/FinancialComplianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complianceAI.financialCompliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialComplianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
